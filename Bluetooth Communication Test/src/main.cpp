052fd34c4a1f50178d581eefb14ad22db736fe17
Jordy Yeoman <jordyyeoman@gmail.com>
2022-08-23 21:45:10 +0800

    Work through some issues

--- Bluetooth Communication Test/src/main.cpp
+++ Bluetooth Communication Test/src/main.cpp
@@ -3,64 +3,30 @@
 #include <BLEServer.h>
 #include <Arduino.h>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-void doShit(uint8_t * theSamePayloadBra, size_t length) {
-  String message; 
-  int msgCharIndex;
-
- for(int i = 0; i <= length; i++) {
-      if((char)theSamePayloadBra[i] == ':' && i != msgCharIndex) {
-          msgCharIndex = i + 2; continue; // Plus 2 since the message is ' :"testMsg" '
-      }
-      if(i == msgCharIndex) {
-          char Ltr = (char)theSamePayloadBra[i];
-          if(Ltr == '\"') break;
-          message += Ltr;
-          msgCharIndex += 1;
-      }
-  }
-  Serial.println(" ");
-  Serial.println("---MESSAGE---");
-  Serial.println(message);
-  Serial.println("---MESSAGE---");
-  if(message == "on"){
-      // Turn onboard led on
-      digitalWrite(2, HIGH);
-      
-  } else {
-      // Turn onboard led on
-      digitalWrite(2, LOW);
-  }
-}
-
-
 class MyCallbacks: public BLECharacteristicCallbacks
 {
-  void onWrite(BLECharacteristic *pCharacteristic)
+  void onWrite(BLECharacteristic *pCharacteristic, esp_ble_gatts_cb_param_t* param)
   {
-    uint8_t * value = pCharacteristic->getData();
-
-    if (pCharacteristic->getLength() > 0)
+    std::string value = pCharacteristic->getValue();
+    String message;
+    
+    if (value.length() > 0)
     {
-      Serial.println("*********");
-      doShit(value, pCharacteristic->getLength());
-      // Serial.print("New value: ");
-      // for (int i = 0; i < value.length(); i++)
-      // {
-      //   Serial.print(value[i]);
-      // }
-
-      // Serial.println();
-      Serial.println("*********");
+      for (int i = 0; i < value.length(); i++)
+      {
+        char letter = value[i];
+        message += letter;
+      }
     }
+    Serial.println("MESSAGE: ");
+    Serial.print(message);
   }
 };
 
+// NOTE: - Be sure to use the correct app when communicating, BLE Scanner works well for sending strings/hex messages without chunking the data.
 void setup()
 {
   Serial.begin(115200);
@@ -69,7 +35,6 @@ void setup()
   Serial.println("2- Scan for BLE devices in the app");
   Serial.println("3- Connect to ESP32-BLE_Server");
   Serial.println("4- Go to CUSTOM CHARACTERISTIC in CUSTOM SERVICE and write something");
-  Serial.println("5- See the magic =)");
 
   BLEDevice::init("ESP32-BLE-Server");
   BLEServer *pServer = BLEDevice::createServer();
@@ -84,7 +49,7 @@ void setup()
 
   pCharacteristic->setCallbacks(new MyCallbacks());
 
-  pCharacteristic->setValue("Hello World");
+  pCharacteristic->setValue("Online and ready sir");
   pService->start();
 
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
@@ -93,5 +58,5 @@ void setup()
 
 void loop()
 {
-  delay(2000);
+  delay(3);
 }
