4b182ff0847eb451630d102cca14276b49f1ac61
Jordy Yeoman <jordyyeoman@gmail.com>
2022-10-18 18:14:18 +0800

    Add secondary ecg monitor

---
+++ Secondary Heart Monitor/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+int sensorValue = 0;
+
+void setup() {
+  pinMode (2, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // Serial.println("LED ON");
+  digitalWrite(2, HIGH);
+  // delay(100);
+
+  // read the analog in value
+  sensorValue = analogRead(A0);
+  // Serial.print("sensor = ");
+  Serial.println(sensorValue);
+  // delay(100);
+
+  // Serial.println("LED OFF");
+  digitalWrite(2, LOW);
+  // delay(100);
+}
