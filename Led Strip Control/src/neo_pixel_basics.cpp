c315419c702b9f79f5adafa530e1333c062fe944
Jordy Yeoman <jordyyeoman@gmail.com>
2022-11-18 16:24:11 +0800

    Further led strip controls + simple control loops

--- Led Strip Control/src/neo_pixel_basics.cpp
+++ Led Strip Control/src/neo_pixel_basics.cpp
@@ -1,24 +1,31 @@
 #include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
 
 int data_pin = 6;
 int num_pixels = 5;
+int delay_time = 100;
+// Remember prev led state
+int led_toggle_pin = 3;
+int btn_pin = 2;
+int btn_state = LOW;
+int btn_prev = HIGH;
+int led_state = LOW;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(num_pixels, data_pin, NEO_GRB + NEO_KHZ800); // Speed at which to communicate with the leds
 
 void setup()
 {
+    // Add serial debugging
+    Serial.begin(9600);
     // Initialize Neopixels
     pixels.begin();
+    // Led toggle switch
+    // Set up pins
+    pinMode(btn_pin, INPUT);
+    pinMode(led_toggle_pin, OUTPUT);
 
-    // Set LED colors
-    pixels.setPixelColor(0, pixels.Color(255, 0, 0));
-    pixels.setPixelColor(1, pixels.Color(255, 127, 0));
-    pixels.setPixelColor(2, pixels.Color(0, 255, 0));
-    pixels.setPixelColor(3, pixels.Color(0, 0, 255));
-    pixels.setPixelColor(4, pixels.Color(127, 0, 255));
-
-    // Write data out to neopixels
-    pixels.show();
+    // Set LED to default off
+    digitalWrite(led_toggle_pin, LOW);
 }
 
 void updatePixel(int pixelNumber, uint32_t color)
@@ -26,31 +33,52 @@ void updatePixel(int pixelNumber, uint32_t color)
     pixels.setPixelColor(pixelNumber, color);
 }
 
-int getRandomNumber(int index)
+void loopForward()
 {
-    return 10 * index + 1;
-}
-
-void loop()
-{
-    for (int i = 0; i < num_pixels; i++)
+    for (int i = 1; i < num_pixels; i++)
     {
         updatePixel(i, pixels.Color(255, 0, 0));
         updatePixel(i - 1, pixels.Color(0, 0, 0));
         pixels.show();
-        if (i < num_pixels)
-        {
-            delay(250);
-        }
+        delay(delay_time);
     }
-    for (int k = num_pixels; k > 0; k--)
+}
+
+void loopBackward()
+{
+    for (int i = num_pixels - 2; i >= 0; i--)
     {
-        updatePixel(k, pixels.Color(255, 0, 0));
-        updatePixel(k + 1, pixels.Color(0, 0, 0));
+        updatePixel(i, pixels.Color(255, 0, 0));
+        updatePixel(i + 1, pixels.Color(0, 0, 0));
         pixels.show();
-        if (k > num_pixels)
-        {
-            delay(250);
-        }
+        delay(delay_time);
     }
+}
+
+void loop()
+{
+    // Read current button state
+    btn_state = digitalRead(btn_pin);
+    Serial.println(btn_state);
+    if ((btn_prev == HIGH) && (btn_state == LOW))
+    {
+
+        // Toggle the LED
+        if (led_state == LOW)
+        {
+            led_state = HIGH;
+        }
+        else
+        {
+            led_state = LOW;
+        }
+        digitalWrite(led_toggle_pin, led_state);
+    }
+
+    // Remember the previous button state for the next loop iteration
+    btn_prev = btn_state;
+
+    //
+    loopForward();
+    loopBackward();
 }
