940c14ff1e95c068db9732d1f6ada4b87ddb0dce
Jordy Yeoman <jordyyeoman@gmail.com>
2022-11-21 11:40:46 +0800

    Simple led potentiometer circuit

--- Duinotech led ring/src/main.cpp
+++ Duinotech led ring/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 
-void setup() {
+const int led_pin = 33;
+
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(led_pin, OUTPUT);
+  analogWrite(led_pin, 255);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// the loop routine runs over and over again forever:
+void loop()
+{
+  // read the input on analog pin GIOP36:
+  int analogValue = analogRead(26);
+  // Rescale to potentiometer's voltage (from 0V to 3.3V):
+  float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
+
+  // print out the value you read:
+  Serial.print("Analog: ");
+  Serial.print(analogValue);
+  Serial.print(", Voltage: ");
+  Serial.println(voltage);
+  delay(50);
 }
