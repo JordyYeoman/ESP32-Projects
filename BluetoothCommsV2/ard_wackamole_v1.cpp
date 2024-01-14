9d65b2bd40a454e3a8356d3e9e3e3e6c8b65101e
Jordy Yeoman <jordyyeoman@gmail.com>
2024-01-14 08:18:40 +0800

    feat: adding button press functions

--- BluetoothCommsV2/ard_wackamole_v1.cpp
+++ BluetoothCommsV2/ard_wackamole_v1.cpp
@@ -1,10 +1,19 @@
+// TODO:
+// 1. Measure average reaction time over course of game
+// 2. Record game time
+// 3. Record scores
+// 4. Include start button
+// 5. ....
+
 // Temporarily store button value
 int analogValue = 0;
+bool gameStarted = false;
+long randNumber = 0;
 int button_values[] = {913, 431, 269, 181, 111};
 int led_pin_map[] = {7, 2, 3, 4, 5};
 int total_leds = 5;
-int button_tolerance = 20;    // How much +/- value to accept for button resistance when pressed.
-int current_led_index_on = 0; // Between 0 and 4
+int button_tolerance = 20; // How much +/- value to accept for button resistance when pressed.
+bool hasPressedPreviousButton = false;
 
 void setup()
 {
@@ -19,31 +28,36 @@ void setup()
     pinMode(A0, INPUT);
     // Start serial monitoring
     Serial.begin(9600);
-    // Start initial LED
+    // Setup random seed
+    randomSeed(analogRead(A1));
 }
 
 void loop()
 {
-    Serial.print("Active LED index: ");
-    Serial.println(current_led_index_on);
-    digitalWrite(current_led_index_on, HIGH);
-    // digitalWrite(random(1, 5), HIGH);
-    Serial.println("Reading Analog Value...");
-    analogValue = analogRead(A0);
+    // Check if button has been pressed since previous loop
+    // If it HAS, then change to a new random number
+    if (hasPressedPreviousButton)
+    {
+        randNumber = random(total_leds);
+    }
+
+    // Reset boolean check every loop
+    hasPressedPreviousButton = false;
+
+    // Light up current LED
+    digitalWrite(led_pin_map[randNumber], HIGH);
+
     // Find which button was pressed
+    analogValue = analogRead(A0);
     for (int i = 0; i < total_leds; i++)
     {
+        // Match resistance to button pressed
         if (analogValue < button_values[i] + button_tolerance and analogValue > button_values[i] - button_tolerance)
         {
-            Serial.print("Button ");
-            Serial.print(i);
-            Serial.print(" Pressed");
-            digitalWrite(led_pin_map[i], HIGH);
-        }
-        else
-        {
+            // If index of button matches LED rand index, turn it off
+            // and reset boolean toggle
             digitalWrite(led_pin_map[i], LOW);
+            hasPressedPreviousButton = true;
         }
     }
-    Serial.println("");
 }
