d6880b61f3fcd3b5d04aa407d0fdb614ea0749aa
Jordy Yeoman <jordyyeoman@gmail.com>
2024-01-26 10:24:00 +0800

    feat: wackamole with DIY shift outputs MSB , LSB

--- BluetoothCommsV2/leds/wackamolev2.ino
+++ BluetoothCommsV2/leds/wackamolev2.ino
@@ -1,75 +1,76 @@
 int latchPin = 10; // (11) ST_CP [RCK] on 74HC595
 int clockPin = 11; // (9) SH_CP [SCK] on 74HC595
 int dataPin = 9;   // (12) DS [S1] on 74HC595
-int analogPinVal = 0;
-int button_values[] = {481, 311, 227, 176, 141, 114, 89, 60}; // Pin values read by analogue input - A0
-int tolerance = 15;
-int numLeds = 8;
-int binaryNum = 0b00000000;
-long randNumber;
-byte leds = 0;
-
-// Game State
-bool hasPreviouslyPressedButton = false;
-bool newGame = true;
-
-void updateShiftRegisterWithValue(const uint8_t value)
-{
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, value);
-    digitalWrite(latchPin, HIGH);
-}
+int totalLedsPerRow = 8;
 
 void setup()
 {
-    pinMode(latchPin, OUTPUT);
+    // Setup the pins as OUTPUT
     pinMode(dataPin, OUTPUT);
     pinMode(clockPin, OUTPUT);
-    randomSeed(analogRead(A1)); // Take random analog input as seed.
-    Serial.begin(9600);
+    pinMode(latchPin, OUTPUT);
+}
+
+// Function to send a byte to the shift register (LSB first)
+void shiftOutLSB(byte data)
+{
+    // Iterate through each bit in the byte
+    for (int i = 0; i < 8; ++i)
+    {
+        // Shift the bits out, starting from the least significant bit
+        digitalWrite(dataPin, (data & (1 << i)) ? HIGH : LOW);
+
+        // Pulse the clock pin to move to the next bit
+        digitalWrite(clockPin, HIGH);
+        digitalWrite(clockPin, LOW);
+    }
+}
+
+// Function to send a byte to the shift register (MSB first)
+void shiftOutMSB(byte data)
+{
+    // Iterate through each bit in the byte
+    for (int i = 7; i >= 0; --i)
+    {
+        // Shift the bits out, starting from the most significant bit
+        digitalWrite(dataPin, (data & (1 << i)) ? HIGH : LOW);
+
+        // Pulse the clock pin to move to the next bit
+        digitalWrite(clockPin, HIGH);
+        digitalWrite(clockPin, LOW);
+    }
+}
+
+// Function to update the daisy-chained shift registers
+void updateShiftRegisters(byte data1, byte data2, byte data3, byte data4)
+{
+    // Start the data transfer
+    digitalWrite(latchPin, LOW);
+
+    // Send data to each shift register
+    shiftOutLSB(data1);
+    shiftOutLSB(data2);
+    shiftOutLSB(data3);
+    shiftOutLSB(data4);
+
+    // End the data transfer by pulsing the latch pin
+    digitalWrite(latchPin, HIGH);
+    digitalWrite(latchPin, LOW);
 }
 
 void loop()
 {
-    // update random number, if game started || previously
-    if (newGame || hasPreviouslyPressedButton)
-    {
-        randNumber = random(0, numLeds);
-        newGame = false;
-        hasPreviouslyPressedButton = false;
-        //
-        // Update Hardware Display to user
-        //
-        //
-        // For our binary number, bitSet turns the value ON - which means setting the bit to HIGH.
-        // Eg - bitSet(num, 3); will set the bit at index 3 to HIGH (On).
-        bitSet(binaryNum, randNumber);
-        updateShiftRegisterWithValue(binaryNum);
-    }
+    // Loop through from one side to the other of leds
+    // NOTE:
+    // - row 1 is the final shiftRegister (4) to update.
+    updateShiftRegisters(0b10000000, 0b01000000, 0b10000000, 0b01000000);
+    delay(1000);
+    updateShiftRegisters(0b01000000, 0b10000000, 0b01000000, 0b10000000);
+    delay(1000);
 
-    //
-    // Handle Button Press
-    //
-    analogPinVal = analogRead(A0); // read the input pin
-                                   // Find which button was pressed
-    unsigned j = 0;                // keep track of index
-    for (int i : button_values)
-    {
-        Serial.println(j);
-        Serial.println(randNumber);
-        if (analogPinVal > i - tolerance && analogPinVal < i + tolerance)
-        {
-            // Set button to OFF & record time
-            if (j == randNumber)
-            {
-                hasPreviouslyPressedButton = true;
-                // Update score
-                // ...
-                // reset game state to OFF for next LED to light up
-                binaryNum = 0b00000000;
-                updateShiftRegisterWithValue(binaryNum);
-            }
-        }
-        j++;
-    }
+    // Example usage: update shift registers with specific data
+    // updateShiftRegisters(0b10101010, 0b11001100, 0b11110000, 0b00001111);
+
+    // You can modify the data patterns as needed for your application
+    // delay(1000);  // Delay for demonstration purposes
 }
