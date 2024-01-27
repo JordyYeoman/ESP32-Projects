0b0deb856fd6d2c3a610c805f7aebd4ba1e1b562
Jordy Yeoman <jordyyeoman@gmail.com>
2024-01-27 14:01:20 +0800

    feat: utility methods

--- BluetoothCommsV2/leds/wackamolev2.ino
+++ BluetoothCommsV2/leds/wackamolev2.ino
@@ -64,15 +64,18 @@ void updateShiftRegisters(byte data1, byte data2, byte data3, byte data4)
     digitalWrite(latchPin, LOW);
 }
 
-void shiftRegisterAnimation(int indx, bool shouldReset)
+void resetBinaryNums()
 {
-    Serial.println(indx);
-    Serial.println(shouldReset);
     // reset binaryNums for shift registers
     binaryNumShiftRegister1 = resetBinaryNum;
     binaryNumShiftRegister2 = resetBinaryNum;
     binaryNumShiftRegister3 = resetBinaryNum;
     binaryNumShiftRegister4 = resetBinaryNum;
+}
+
+void shiftRegisterAnimation(int indx, bool shouldReset)
+{
+    resetBinaryNums();
     // count forward
     bitSet(binaryNumShiftRegister1, indx);
     bitSet(binaryNumShiftRegister2, indx);
@@ -86,7 +89,8 @@ void shiftRegisterAnimation(int indx, bool shouldReset)
     }
 }
 
-void loop()
+// Animation #1
+void loopForwardAndBackWard()
 {
     for (int i = 0; i < 8; i++)
     {
@@ -97,6 +101,84 @@ void loop()
     {
         shiftRegisterAnimation(i, i == 7);
     }
+}
+
+// Pass in a reference to the variables in the parent function scope.
+void rowIndexUpdate(const byte &shiftRegister, int &idx, bool &isBackwards)
+{
+    // set the new val
+    bitSet(binaryNumShiftRegister1, idx);
+
+    // Check for looping backwards
+    if (isBackwards)
+    {
+        idx--;
+    }
+    else
+    {
+        idx++;
+    }
+    if (idx > 7)
+    {
+        isBackwards = true;
+    }
+    if (idx == 0)
+    {
+        isBackwards = false;
+    }
+};
+
+// Animation #2
+void simpleOffsetStepAnimation()
+{
+    // keep the current animation state
+    int row1Index = 0;
+    bool row1Backwards = false;
+    int row2Index = 0;
+    bool row2Backwards = false;
+    int row3Index = 0;
+    bool row3Backwards = false;
+    int row4Index = 0;
+    bool row4Backwards = false;
+
+    for (int i = 0; i < 32; i++)
+    {
+        // reset to all binary nums LOW
+        resetBinaryNums();
+
+        // start first row animation
+        rowIndexUpdate(binaryNumShiftRegister1, row1Index, row1Backwards);
+
+        // start second animation
+        if (i > 0)
+        {
+            rowIndexUpdate(binaryNumShiftRegister2, row2Index, row2Backwards);
+        }
+
+        // start third row animation
+        if (i > 1)
+        {
+            rowIndexUpdate(binaryNumShiftRegister3, row3Index, row3Backwards);
+        }
+
+        // start fourth row animation
+        if (i > 2)
+        {
+            rowIndexUpdate(binaryNumShiftRegister4, row4Index, row4Backwards);
+        }
+
+        updateShiftRegisters(binaryNumShiftRegister1, binaryNumShiftRegister2, binaryNumShiftRegister3, binaryNumShiftRegister4);
+        delay(150);
+    }
+}
+
+void loop()
+{
+    // Simple animation
+    // loopForwardAndBackWard();
+
+    // Simple off timing animation
+    simpleOffsetStepAnimation();
 
     // Example usage: update shift registers with specific data
     // updateShiftRegisters(0b10101010, 0b11001100, 0b11110000, 0b00001111);
